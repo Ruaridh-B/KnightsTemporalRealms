5eab06432cd5efa2af99beffffd718a1be444450
Ruaridh-B <158184092+Ruaridh-B@users.noreply.github.com>
2024-04-24 12:40:32 +0100

    1

--- imgtran.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('DOMContentLoaded', function() {
-    var images = document.querySelectorAll('.image-container img');
-
-    images.forEach(function(img) {
-        img.addEventListener('load', function() {
-            img.style.opacity = 1;
-        });
-    });
-});
